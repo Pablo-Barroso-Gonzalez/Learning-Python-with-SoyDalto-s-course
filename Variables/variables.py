e190481eaf1786682beb93c83991b09d44ba1e08
Pablo Barroso González <pablobarrosogonzalez2@gmail.com>
2026-07-13 14:38:03 +0200

    Mejora estrucutra y insercion de comentarios.

--- Variables/variables.py
+++ Variables/variables.py
@@ -1,3 +1,11 @@
-nombre = "pablo"
-print(nombre)
-a = 3
+#Definir variables
+nombre = "Pablo"
+edad= 16
+nombre_usuario = "Juan"
+
+saludo = "Hola, " + nombre_usuario +" como estas?" 
+print(saludo)
+
+#String con suma
+presentacion = f"Mi nombre es {nombre} y tengo {edad} años."
+print(presentacion)
