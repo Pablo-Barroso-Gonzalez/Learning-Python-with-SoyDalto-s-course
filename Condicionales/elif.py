f2f6395250892b46d13ccda0c7a70cdc28cb563a
Pablo Barroso González <pablobarrosogonzalez2@gmail.com>
2026-07-15 00:19:13 +0200

    Mini script q comprueba si un usuario se pude registrar en un torneo

---
+++ Condicionales/elif.py
@@ -0,0 +1,10 @@
+nombre = "Pablo"
+edad = 16
+nivel = 35
+victorias = 80
+baneado = False
+
+if edad >= 16:
+    print("Pudes entrar")
+else:
+    print("Debes de ser mayor de 16 para poder participar en el torneo")
