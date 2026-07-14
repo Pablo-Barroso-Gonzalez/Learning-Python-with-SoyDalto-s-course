4f491c6423ce9e6e0e32404d55b95f147817c250
Pablo Barroso González <pablobarrosogonzalez2@gmail.com>
2026-07-14 22:07:42 +0200

    Apartado operadores arimeticos acabado

---
+++ operdadores/op_arimetico.py
@@ -0,0 +1,21 @@
+## Suma
+suma = 77 + 33
+print(suma)
+## Resta
+resta = 48 - 32
+print(resta)
+## Multiplicacion
+mulitplicacion = 4 * 6
+print(mulitplicacion)
+## Exponente
+exponente = 2 ** 5
+print(exponente)
+## Division simple (always float)
+division = 12 / 6
+print(division)
+## Division redondeada (hacia abajo) y lo convierte en un int
+division_redondeada = 58 // 8
+print(division_redondeada)
+## Resto
+resto = 43 % 3
+print(resto)
