d839e712a2caa8c0348b34ff35378cdea4bfdc1f
Pablo Barroso González <pablo.barroso@proton.me>
2026-07-15 18:03:49 +0200

    Añadiendo operadores logicos

---
+++ operdadores/op_logicos.py
@@ -0,0 +1,34 @@
+
+#And
+resultado = True and True # Devuelve True
+resultado1 = False and False 
+resultado2 = True and False
+resultado3 = False and True
+#OR
+
+resultado4 = True or True
+resultado5 = False or False
+resultado6 = True or False
+resultado7 = False or True
+
+#not
+resultado8 = not True
+resultado9 = not False
+
+print(f"""
+AND
+True and True = {resultado}
+False and False = {resultado1}
+True and False = {resultado2}
+False and True = {resultado3}
+
+OR
+True or True = {resultado4}
+False or False = {resultado5}
+True or False = {resultado6}
+False or True = {resultado7}
+
+NOT
+not True = {resultado8}
+not False = {resultado9}
+""")
