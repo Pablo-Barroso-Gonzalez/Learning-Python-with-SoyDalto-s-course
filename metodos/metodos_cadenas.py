59ec85c53678709894b69a3f04ab43787434fd51
Pablo Barroso González <pablo.barroso@proton.me>
2026-07-16 01:26:35 +0200

    metodos de cadenas lower upper y capitalize aprendido

---
+++ metodos/metodos_cadenas.py
@@ -0,0 +1,20 @@
+# Definiendo variables
+cadena1 = "hola SOY Pablo"
+cadena2 = "Bienvenido m aquinola"
+
+# Convierte a mayusculas.
+mayusculas = cadena1.upper()
+
+# Convierte a minusculas.
+minusculas = cadena1.lower()
+
+# Convierte primera letra en mayuscula.
+cadena1 = cadena1.capitalize()
+print(cadena1)
+
+#Buscamos una cadena en otra cadena
+busqueda_find = cadena1.find("s")
+print(busqueda_find)
+
+
+
