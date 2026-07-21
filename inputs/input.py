faa14aaa4938cafec1d5834bd45953ffedd0dee4
Pablo Barroso González <pablo.barroso@proton.me>
2026-07-21 23:49:31 +0200

    Seccion inputs completada

---
+++ inputs/input.py
@@ -0,0 +1,2 @@
+nombre = input("Cual es tu nombre:\n")
+print(f"Tu nombre es {nombre}")
---
+++ inputs/input_numeros.py
@@ -0,0 +1,4 @@
+numero = input("Dame un numero porfavor:\n")
+
+resultado = f"El doble de ese numero es {int(numero) * 2} pero el doble exacto de ese numero es {float(numero) * 2}"
+print(resultado)
