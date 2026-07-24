655e827750c59cc49dd245e1b2676034bab29287
Pablo Barroso González <pablo.barroso@proton.me>
2026-07-24 07:42:23 +0200

    añadiendo aprendizaje desempaquetamiento

---
+++ variables_2.0/desempaquetado.py
@@ -0,0 +1,4 @@
+lista = ["Pablo","Barroso","España","informatica","ciberseguridad"]
+tupla = ("Pablo","Barroso","España","informatica","ciberseguridad")
+nombre,apellido,pais,hobby,especialidad = git 
+print(f"El sujeto se llama {nombre} {apellido}, vive en {pais}.Su pasion y hobby es la {hobby} concentramente le gusta mucho la especializacion de {especialidad}.")
