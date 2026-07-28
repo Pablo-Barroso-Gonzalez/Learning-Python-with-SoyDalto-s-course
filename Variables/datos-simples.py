eaff021808213c1fddff4483a70551ed2dbd09eb
Pablo Barroso González <pablo.barroso@proton.me>
2026-07-28 12:49:16 +0200

    cambiando comentario presentacion

--- Variables/datos-simples.py
+++ Variables/datos-simples.py
@@ -3,9 +3,10 @@ nombre = "Pablo"
 edad= 16
 nombre_usuario = "Juan"
 
+#Saludo usuario
 saludo = "Hola, " + nombre_usuario +" como estas?" 
 print(saludo)
 
-#String con suma
+#Presentacion
 presentacion = f"Mi nombre es {nombre} y tengo {edad} años."
-print(presentacion)
+print(presentacion) 
