649aaa15a8d2b1b14e797f4b7697fe61c773f748
Pablo Barroso González <pablo.barroso@proton.me>
2026-07-28 12:47:25 +0200

    Eliminando el print

--- operdadores/op_logicos.py
+++ operdadores/op_logicos.py
@@ -15,20 +15,3 @@ resultado7 = False or True
 resultado8 = not True
 resultado9 = not False
 
-print(f"""
-AND
-True and True = {resultado}
-False and False = {resultado1}
-True and False = {resultado2}
-False and True = {resultado3}
-
-OR
-True or True = {resultado4}
-False or False = {resultado5}
-True or False = {resultado6}
-False or True = {resultado7}
-
-NOT
-not True = {resultado8}
-not False = {resultado9}
-""")
