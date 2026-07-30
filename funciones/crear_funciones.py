2cdf5327ce93e2f2f0422b1720a6e2de6189b554
Pablo Barroso González <pablo.barroso@proton.me>
2026-07-31 00:10:42 +0200

    Creando funcion generadora de contraseñas a partir de un numero

---
+++ funciones/crear_funciones.py
@@ -0,0 +1,40 @@
+def saludar(nombre,genero):
+    genero = genero.lower()
+    
+    if genero == "hombre":
+        adjetivo = "rey"
+        
+    elif genero == "mujer":
+        adjetivo = "diva"
+    
+    else:
+        adjetivo = "amor"
+        
+    print(f"Hola {nombre}, mi {adjetivo} que tal.")
+ 
+saludar("Pablo","hombre")
+saludar("Emma","MUjer")
+saludar("Camila","binario")
+
+def generar_contraseña(num):
+    chars = "abcdefghij"
+    numero_entero = str(num)
+    primer_digito = int(numero_entero[0])
+    char1 = (primer_digito - 2) % len(chars)
+    char2 = (primer_digito) % len(chars)
+    char3 = (primer_digito + 3) % len(chars)
+    char4 = (primer_digito + 7) % len(chars)
+    
+    contraseña = (
+        f"{chars[char1]}"
+        f"{chars[char2]}"
+        f"{chars[char3]}"
+        f"{chars[char4]}"
+        )
+    
+    return contraseña
+
+num = 832
+
+password = generar_contraseña(num)
+print(f"La contraseña genereada a partir de {num} es {password}.")
