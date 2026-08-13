1a638e84b75a6817d3f5728e0a49d7bcb49c9911
Pablo Barroso González <pablo.barroso@proton.me>
2026-08-13 23:50:23 +0200

    Ejercicio secuencia de fibonacci completada

---
+++ Ejercicos-2/Ejercicio-2.3/ejercicio2.3.py
@@ -0,0 +1,25 @@
+#creando funcion q muestre la serie de fibonacci desde 0 al numero dado
+
+
+
+
+def crear_fibonacci(limite):
+    
+    sucesión_Fibonacci = [0]
+    
+    a = 0
+    b = 1
+    
+    while limite > a + b:
+        temporal_a = a
+        a = b
+        b = temporal_a + b
+        sucesión_Fibonacci.append(b)
+    return sucesión_Fibonacci
+
+limite = int(input(f"Introduzca el numero limite para la serie de fibonacc:\n"))
+
+resultado = crear_fibonacci(limite)
+
+print(f"La sucesión de Fibonacci con limite de {limite} da como resultado:\n{resultado}")
+
