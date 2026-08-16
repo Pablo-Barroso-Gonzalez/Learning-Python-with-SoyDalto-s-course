49c4a135377a3bd7a3633f1538843190bc274956
Pablo Barroso González <pablo.barroso@proton.me>
2026-08-16 20:52:46 +0200

    escribiendo ejemplo de como escribir en un txt

--- archivos/escribir_txt.py
+++ archivos/escribir_txt.py
@@ -1,5 +1,8 @@
-archivo = open("archivos/documento2.txt")
+with open ("archivos/documento.txt","w") as archivo:
+    archivo.write("Los perros son animales.\n")
+    archivo.write("La odisea es un gran obra.\n")
+    archivo.write("Los delfines son muy inteligentes.\n")
 
-with open("archivos/documento2.txt") as archivo:
+with open("archivos/documento.txt","r") as archivo:
     archivo = archivo.read()
     print(archivo)
