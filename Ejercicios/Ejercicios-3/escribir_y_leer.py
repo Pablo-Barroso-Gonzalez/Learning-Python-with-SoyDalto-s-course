03fc0e7640d6430e13f1c7f6f8af2dcbbb5102ab
Pablo Barroso González <pablo.barroso@proton.me>
2026-08-16 14:30:23 +0200

    ejercicio uno completado de manejo arachivos .txt

---
+++ Ejercicios/Ejercicios-3/escribir_y_leer.py
@@ -0,0 +1,11 @@
+#Crear archivo txt o abrirlo si existe
+with open ("Ejercicios/Ejercicios-3/prueba.txt","w",encoding="UTF-8") as archivo:
+    archivo = archivo.write("Escribiendo en el archivo de prueba.")
+
+#Lo vovemos a abrir con el permiso de r de read y lo guardamos en una variable
+with open ("Ejercicios/Ejercicios-3/prueba.txt","r",encoding="UTF-8") as archivo:
+    archivo = archivo.read()
+    print(dir(archivo))
+print(archivo)
+
+print(dir(archivo))
